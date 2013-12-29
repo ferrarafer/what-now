1aa83dbb72453079993070dea4519c9d3bd6f226
AlphaGit <alphagma@gmail.com>
2013-12-29 11:08:48 -0600

    Yeoman setup.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('whatNowApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
