e0e48e344a734ce1a757b348d8bef115738f6517
Alpha <alphagma@gmail.com>
2014-01-14 16:24:29 -0600

    Added application files to the watched set of files that runs tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,7 +32,10 @@ module.exports = function (grunt) {
         tasks: ['newer:jshint:all']
       },
       jsTest: {
-        files: ['test/spec/{,*/}*.js'],
+        files: [
+          'test/spec/{,*/}*.js',
+          '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js'
+        ],
         tasks: ['newer:jshint:test', 'karma']
       },
       compass: {
