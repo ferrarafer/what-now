e74f434427f842dfe639f26cdbfb66a6fd511f3a
Alpha <alphagma@gmail.com>
2014-02-10 20:39:26 -0600

    Drawing tasks as circles.

--- app/scripts/directives/d3Pert.js
+++ app/scripts/directives/d3Pert.js
@@ -7,22 +7,41 @@ angular.module('whatNowApp.directives', ['d3'])
       scope: {
         taskList: '='
       },
-      link: function (scope, element) {
+      link: function ($scope, element) {
         var svg = d3Service.select(element[0]).append('svg');
 
         svg.attr('height', 100)
-           .attr('width', 300);
+           .attr('width', '100%');
 
-        var dataset = [ 5, 10, 15, 20, 25 ];
+        $scope.$watchCollection('taskList', function() {
+          svg.selectAll('*').remove();
 
-        var circles = svg.selectAll('circle')
-          .data(dataset)
-          .enter()
-          .append('circle');
+          var groups = svg.selectAll('g')
+            .data($scope.taskList)
+            .enter()
+            .append('g')
+            .attr('transform', function(d, i) {
+              var x = i * 50 + 25;
+              var y = 50;
+              return 'translate(' + x + ', ' + y + ')';
+            });
 
-        circles.attr('cx', function (d, i) { return i * 50 + 25; })
-          .attr('cy', 50)
-          .attr('r', function (d) { return d; });
+          var circles = groups.append('circle')
+            .attr({
+              cx: 0,
+              cy: 0,
+              r: function(d) { return d.name.length; },
+            });
+
+          var labels = groups.append('text')
+            .text(function(d) { return d.name; })
+            .attr({
+              fill: 'black',
+              'alignment-baseline': 'middle',
+              'text-anchor': 'middle',
+              y: -20
+            });
+        });
       }
     };
   }]);
