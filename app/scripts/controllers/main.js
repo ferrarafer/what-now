dc14524df27c4ad5753e757108c5e5267132b274
Alpha <alphagma@gmail.com>
2014-02-23 13:45:21 -0600

    Dependency resolution being used in main controller.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,53 +1,55 @@
 'use strict';
 
 angular.module('whatNowApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.tasks = [];
-    $scope.newTask = { id: 0 };
+  .controller('MainCtrl', ['$scope', 'dependencyResolutionService',
+    function ($scope, dependencyResolutionService) {
+      $scope.tasks = [];
+      $scope.newTask = { id: 0 };
 
-    $scope.addTask = function(task) {
-      $scope.tasks.push(task);
-      $scope.buildGraphDependencies(task);
-    };
+      $scope.addTask = function(task) {
+        $scope.tasks.push(task);
+        $scope.buildGraphDependencies(task);
+      };
 
-    $scope.addTaskFromForm = function() {
-      $scope.addTask($scope.newTask);
-      $scope.newTask = { id: $scope.newTask.id + 1 };
-    };
+      $scope.addTaskFromForm = function() {
+        $scope.addTask($scope.newTask);
+        $scope.newTask = { id: $scope.newTask.id + 1 };
+      };
 
-    $scope.removeTask = function(taskToRemove) {
-      var index = $scope.tasks.length;
-      while (index--) {
-        var task = $scope.tasks[index];
-        if (task === taskToRemove) {
-          $scope.tasks.splice(index, 1);
-        } else {
-          var dependencyIndex = task.dependsOn.length;
-          while (dependencyIndex--) {
-            if (task.dependsOn[dependencyIndex] === taskToRemove) {
-              task.dependsOn.splice(dependencyIndex, 1);
+      $scope.removeTask = function(taskToRemove) {
+        var index = $scope.tasks.length;
+        while (index--) {
+          var task = $scope.tasks[index];
+          if (task === taskToRemove) {
+            $scope.tasks.splice(index, 1);
+          } else {
+            var dependencyIndex = task.dependsOn.length;
+            while (dependencyIndex--) {
+              if (task.dependsOn[dependencyIndex] === taskToRemove) {
+                task.dependsOn.splice(dependencyIndex, 1);
+              }
             }
           }
         }
-      }
-    };
+      };
 
-    $scope.buildGraphDependencies = function(newTask) {
-      newTask.dependsOn = [];
+      $scope.buildGraphDependencies = function(newTask) {
+        newTask.dependsOn = [];
 
-      var ids = newTask.dependsOnText ? newTask.dependsOnText.split(',') : [];
-      
-      for (var idIndex = 0; idIndex < ids.length; idIndex++) {
-        var dependsOnId = parseInt(ids[idIndex], 10);
+        var ids = newTask.dependsOnText ? newTask.dependsOnText.split(',') : [];
+        
+        for (var idIndex = 0; idIndex < ids.length; idIndex++) {
+          var dependsOnId = parseInt(ids[idIndex], 10);
 
-        // search for referenced task and link it
-        var index = $scope.tasks.length;
-        while (index--) {
-          if ($scope.tasks[index].id === dependsOnId) {
-            newTask.dependsOn.push($scope.tasks[index]);
-            break;
+          // search for referenced task and link it
+          var index = $scope.tasks.length;
+          while (index--) {
+            if ($scope.tasks[index].id === dependsOnId) {
+              newTask.dependsOn.push($scope.tasks[index]);
+              break;
+            }
           }
         }
-      }
-    };
-  });
+      };
+    }
+  ]);
